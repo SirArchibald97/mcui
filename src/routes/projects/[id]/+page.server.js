3e30e87296f9e953ad2eb7f02ea79e9e1925b2d6
SirArchibald97 <43316918+SirArchibald97@users.noreply.github.com>
2024-01-29 12:46:37 +0000

    Added create inventory functionality

--- src/routes/projects/[id]/+page.server.js
+++ src/routes/projects/[id]/+page.server.js
@@ -1,10 +1,31 @@
 import { redirect } from '@sveltejs/kit';
+import db from "../../../lib/db.js";
 
-export function load({ cookies, params }) {
+export async function load({ cookies, params }) {
     const user_cookie = cookies.get("user");
     if (!user_cookie) throw redirect(301, "/login");
 
     const user = JSON.parse(user_cookie);
-    const project = user.projects.find((p) => p.id === params.id);
-    return { user, project };
+    const new_user = await db.collection("users").findOne({ email: user.email });
+    const project = new_user.projects.find((p) => p.id === params.id);
+    return { user: JSON.parse(JSON.stringify(new_user)), project };
+}
+
+export const actions = {
+    addinv: async({ cookies, request, params }) => {
+        const user_cookie = cookies.get("user");
+        if (!user_cookie) throw redirect(301, "/login");
+
+        const user = JSON.parse(user_cookie);
+        const new_user = await db.collection("users").findOne({ email: user.email });
+        const project = new_user.projects.find((p) => p.id === params.id);
+
+        const data = await request.formData();
+        if (!Number(data.get("size"))) return { success: false, err_type: "addinv", message: "Invalid size!" }
+        if (![1, 2, 3, 4, 5, 6].includes(Number(data.get("size")))) return { success: false, err_type: "addinv", message: "Invalid size!" }
+
+        project.inventories.push({ title: data.get("title"), size: Number(data.get("size")) * 9, items: [] });
+        await db.collection("users").updateOne({ email: user.email }, { $set: { projects: new_user.projects } });
+        return { body: { success: true } };
+    }
 }
--- src/routes/projects/[id]/+page.svelte
+++ src/routes/projects/[id]/+page.svelte
@@ -1,10 +1,15 @@
 <script>
     export let data;
-    const { user, project } = data;
+    let { user, project } = data;
+
+    export let form;
 
     import Logout from "../../../svgs/Logout.svelte";
     import Back from "../../../svgs/Back.svelte";
     import Add from "../../../svgs/Add.svelte";
+
+    let title = "";
+    let size = "";
 </script>
 
 <main class="h-screen">
@@ -31,16 +36,43 @@
         <!-- sidebar -->
         <div class="flex flex-col bg-slate-700 px-6 text-center shrink-0">
             <p class="text-xl font-bold py-2">{project.title}</p>
-            <!-- new inventory -->
-            <button class="flex flex-row gap-x-2 px-4 py-2 bg-slate-600 border-2 border-slate-400 hover:bg-slate-500 my-2"><Add /><span>New Inventory</span></button>
-            
+
+            <!-- add new inventory -->
+            {#if form?.success === false && form?.err_type === "addinv"}
+                <div class="bg-red-400 border-2 border-red-500 text-red-600 rounded-md">
+                    <p class="text-md font-semibold p-1">{form.message}</p>
+                </div>
+            {/if}
+            <form method="POST" action="?/addinv" class="flex flex-col bg-slate-600 rounded-md my-2">
+                <div class="flex flex-col rounded-md">
+                    <input name="title" type="text" placeholder="Title" bind:value={title} class="bg-slate-600 border-b-2 border-slate-500 focus:border-slate-300 outline-none m-2 px-3 py-1" />
+                </div>
+                <div class="flex flex-row rounded-md">
+                    <input name="size" type="text" bind:value={size} class="bg-slate-600 border-b-2 border-slate-500 focus:border-slate-300 outline-none m-2 px-3 py-1" /><span class="py-1 my-2 pr-2">rows</span>
+                </div>
+                {#if size.length === 0 || title.length === 0}
+                    <button type="submit" disabled class="flex flex-row gap-x-2 px-4 py-2 my-2 bg-slate-500 mx-2 rounded-md"><Add /><span>Add Inventory</span></button>
+                {:else}
+                    <button type="submit" class="flex flex-row gap-x-2 px-4 py-2 my-2 bg-slate-500 hover:bg-slate-400 mx-2 rounded-md"><Add /><span>Add Inventory</span></button>
+                {/if}
+            </form>
+
             <!-- save -->
-            <button class="px-4 py-2 bg-green-500 border-3 border-green-600 hover:bg-green-400 my-2 mb-4">Save Changes</button>
+            <button class="px-4 py-2 bg-green-500 border-3 border-green-600 hover:bg-green-400 my-2 mb-4 rounded-md">Save Changes</button>
         </div>
 
-        <!-- content -->
+        <!-- inventories -->
         <div class="grow">
-            <img src="/images/inventory.png" alt="inventory" class="w-3/5" />
+            {#each project.inventories as inventory}
+                <div>
+                    <p>{inventory.title}</p>
+                </div>
+            {/each}
         </div>
+
+        <!-- item editor -->
+        <div>
+
+        </div>    
     </div>
 </main>
